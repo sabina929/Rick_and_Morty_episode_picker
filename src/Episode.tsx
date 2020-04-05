00f1435b6163521e112f8b3ac36dcef349eface2
Sabina <sabina.abbasova2909@gmail.com>
2020-04-05 16:27:27 +0400

    added single episode page

--- src/Episode.tsx
+++ src/Episode.tsx
@@ -1,14 +1,9 @@
 import React, {FC, useState, useEffect, useCallback} from 'react'
-import {Store} from './Store'
-import {IEpisodeProps, IEpisode} from 'interfaces'
-import {toggleFavAction} from './Actions'
 
 
 const Episode: FC = ({ episodeID }: any ): JSX.Element => {
     // const {state} = React.useContext(Store)
     // console.log(state.episodes.episodeID)
-
-
 
 const useEpisodeFetch:any = (episodeId:number) => {
     
@@ -19,22 +14,50 @@ const useEpisodeFetch:any = (episodeId:number) => {
             const URL = `http://api.tvmaze.com/episodes/${episodeId}`;
             const data= await fetch(URL);
             const dataJSON = await data.json()
-            console.log(dataJSON.id);
+            console.table(dataJSON);
 
-            // setState({
-            //     id: dataJSON.id,
-            //     url: dataJSON.url,
-            //     name: dataJSON.name,
-            //     season: dataJSON.season,
-            //     number: dataJSON.number,
-            //     airdate: dataJSON.airdate,
-            //     airtime: dataJSON.airtime,
-            //     airstamp: dataJSON.airstamp,
-            //     runtime: dataJSON.runtime,
-            //     image: dataJSON.image,
-            //     summary: dataJSON.summary
-            // })
-            setState([...dataJSON])
+            // SUMMARY - TRIMMING TEXT
+            const summaryText = dataJSON.summary
+            // console.table(summaryText);
+            let regexStart = /<p>/g
+            let regexEnd = /<[/]p>/g
+            let textTrimedStart = summaryText.replace(regexStart, '');
+            // console.log(textTrimedStart)
+            let textTrimedEnd = textTrimedStart.replace(regexEnd, '');
+            // console.log(textTrimedEnd)
+            // let tt = summaryText.match(regex)
+            // console.log(tt)
+
+            // IMAGE
+            const EpisodeImg = dataJSON.image.medium
+            // console.log(EpisodeImg);
+
+            // NAME
+            const episodeName = dataJSON.name
+            // console.log(episodeName);
+
+            // RUNTIME
+            const episodeRuntime = dataJSON.runtime
+            // console.log(episodeRuntime);
+
+            // SEASON
+            const se = dataJSON.season
+            // console.log(se);
+
+            // EPISODE
+            const ep = dataJSON.number
+            // console.log(ep);
+
+
+            setState({
+                ...dataJSON,
+                season:se,
+                number:ep,
+                image:EpisodeImg,
+                name:episodeName,
+                runtime:episodeRuntime,
+                summary: textTrimedEnd
+            })
             
         } catch (error) {
             console.log(error);
@@ -48,16 +71,33 @@ const useEpisodeFetch:any = (episodeId:number) => {
 
  
 
-    console.log(state)
+    console.log( state)
     return [state];
 }
 
-const [id] = useEpisodeFetch(episodeID);
-console.log(id)
+const a = useEpisodeFetch(episodeID);
+// console.log(typeof a)
+console.log(a)
+
+const text = a[0].summary;
+// console.log(typeof text)
+// console.log(JSON.stringify(textCopy))
+
+const EImg = a[0].image;
+const EName = a[0].name;
+
+
     return (
-        <div>
-            {id}
-        </div>
+        <section className="episode-section">
+            <div className="ep-img">
+                <img src={EImg} alt={`Rick and Morty ${EName}`}/>
+            </div>
+            <div className="ep-info">
+                <h2>"{EName}"</h2>
+                <p>{text}</p>
+            </div>
+            
+        </section>
     )
 }
 
